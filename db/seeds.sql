127b94f25d55dbd3c817a4fa3927162073b43deb
Ryan Thomas <rythomas2525@gmail.com>
2020-02-23 19:27:33 -0700

    MVC set up

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+
+
+
+INSERT INTO burgers (burger_name) VALUES ("What-a-Burger"), ("In n Out"), ("Five Guys")
